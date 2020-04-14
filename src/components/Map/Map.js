1e8c9f503dbebbc8b38be3d21059756da55419d3
unknown <dibenvishvaa@gmail.com>
2020-04-15 03:07:39 +0800

    removed https from map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -27,7 +27,7 @@ export default class Map extends React.Component {
     })
 
     L.tileLayer(
-      "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+      "http://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
       {
         detectRetina: false,
         maxZoom: L.Browser.retina ? 12 : 11,
