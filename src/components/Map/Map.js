eea8d5451c191feb522e366567e3086b206ebb49
unknown <dibenvishvaa@gmail.com>
2020-04-15 15:38:36 +0800

    add different map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -23,11 +23,10 @@ export default class Map extends React.Component {
     })
 
     L.tileLayer(
-      "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       {
-        maxZoom: 20,
-        attribution:
-          '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         detectRetina: false,
         noWrap: true,
         // maxNativeZoom: L.Browser.retina ? 10 : 11,
